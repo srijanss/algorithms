38b821d8b73ee757f9bb7bbcb31e9899d508d294
Srijan Manandhar <srijan.manandhar@gmail.com>
2018-07-27 10:39:03 +1000

    Added test for day two problem

--- tests/test_problems.py
+++ tests/test_problems.py
@@ -15,4 +15,8 @@ def test_day_one(obj):
     assert obj.day_one([10, 2, 3, 4, 7], 17) == True
     assert obj.day_one([10, 2, 3, 4, 7], 15) == False 
     assert obj.day_one([1, 2, 3, 4, 5], 5) == True
-    assert obj.day_one([1, 2, 3, 5, 5], 10) == True 
+    assert obj.day_one([1, 2, 3, 5, 5], 10) == True
+
+def test_day_two(obj):
+    assert obj.day_two([1, 2, 3, 4, 5]) == [120, 60, 40, 30, 24]
+    assert obj.day_two([4, 1, 6, 3, 2]) == [36, 144, 24, 48, 72]
